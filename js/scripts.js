73758912b9fb3ebb543c853f1e6d4bdce9194991
Louis Knolle <knollelw@gmail.com>
2022-04-18 21:13:59 -0400

    will condense and refactor some of the code thus far

--- js/scripts.js
+++ js/scripts.js
@@ -3,40 +3,27 @@ $(document).ready(function() {
 });
 
 
-function noInputtedWord() {
-  for (let i=0; i < arguments.length; i++) {
-    if (arguments[i].trim().length === 0) {
-      return true;
-    }
-  }
-  return false;
-}
-
 function pigLatin(word) {
-  if (noInputtedWord(word)) {
-    return 0;
-  }
+  if (word.trim().length > 0) {
   const vowels = "aeiou";
-  const firstLetter = word.charAt(0);
-  const secondLetter = word.charAt(1);
   let letters = word.split("");
+  const firstLetter = letters[0];
+  const secondLetter = letters[1];
 
   if(vowels.includes(firstLetter)) {
     return word + "way";
-  } else if (!vowels.includes(letters[0]) && firstLetter === 'q') {
-    // let letters = word.split("");
-    let quWord = "";
-    let twoLetters = letters.slice(2);
+  } else if (firstLetter === 'q' && secondLetter === 'u') {
+    let firstTwoLettersRemoved = letters.slice(2);
     
-    return (twoLetters.join("") + "quay");
+    return (firstTwoLettersRemoved.join("") + "quay");
   } else {
     let firstConsonant = "";
-    let letters = word.split("");
-    for(let i = 0; i < letters.length; i++) {
-      if(!vowels.includes(letters[0])) {
+    for(let i = 0; i < word.length; i++) {
+      if(!vowels.includes(firstLetter)) {
           firstConsonant += letters.shift();
       }
     }
     return (letters.join("") + firstConsonant + "ay");
   }
 } 
+}
